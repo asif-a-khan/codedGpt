b5d3c9ccf272f084dc93eb70ebce7495e8fbaf84
Asif Khan <asif14.k@hotmail.com>
2025-02-26 17:27:39 -0500

    Changed component to render on client due to netlify build issues

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,24 +1,36 @@
+"use client"; // Important to use client side rendering.
 import LoginButton from "@/components/Buttons/LoginButton";
-async function getProviders() {
-  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/providers`);
+import { useState, useEffect } from "react";
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch providers");
+export default function SignIn() {
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      try {
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/providers`);
+        if (!res.ok) {
+          throw new Error("Failed to fetch providers");
+        }
+        const data = await res.json();
+        setProviders(data);
+      } catch (error) {
+        console.error("Error fetching providers:", error);
+      }
+    };
+    fetchProviders();
+  }, []);
+
+  if (!providers) {
+    return <p>Loading...</p>;
   }
-
-  return res.json();
-}
-
-export default async function SignIn() {
-  const resp: ReturnType<typeof getProviders> = (await getProviders()) || {};
 
   return (
     <div className="flex min-h-screen flex-col items-center mx-auto p-24 max-w-[40rem]">
       <h1 className="font-bold text-3xl">Sign in to your account</h1>
-
       <div className="flex flex-col items-center gap-y-4 pt-5">
-        {Object.values(resp).map(
-          (provider) =>
+        {Object.values(providers).map(
+          (provider: any) =>
             provider.id !== "hasura-credentials" && (
               <LoginButton auth={provider} key={provider.id} />
             )
